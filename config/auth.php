f310955aa83ae4136624381bc4c03b5ef97fb908
Shifat Nipa <shifatnipa229@gmail.com>
2023-02-04 12:34:21 +0600

    from data save,data show, data update and delete

--- config/auth.php
+++ config/auth.php
@@ -41,10 +41,9 @@ return [
             'provider' => 'users',
         ],
         'api' => [
-            'driver' => 'jwt',
+            'driver' => 'token',
             'provider' => 'users',
-            'hash' => true,
-        ],
+        ]
     ],
 
     /*
